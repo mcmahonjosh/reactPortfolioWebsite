547e4753166e5847f5bfaf507fc1e9200e4b1be6
Wizard10fun <wisard10fun@gmail.com>
2024-06-28 01:14:15 -0400

    Improvements to Contact Page -added linked gmail and and github images - images are functional links

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ContactComponent from "./ContactComponent";
+import imageSrc from './img/github-logo-image.png'; // Import your image
+import imageSrc2 from './img/gmail-logo-image.png';
+import imageSrc3 from './img/new-linkedin-image.png';
 
 
 const ContactPage = () => {
     return ( 
-        <ContactComponent />
+        <div className="contactpage">
+          <h1 className="slideheaders">Contact Me</h1>
+          <ContactComponent />
+          <a href="https://github.com/mcmahonjosh" target="_blank" rel="noopener noreferrer">
+                <img src={imageSrc} alt="GitHub Profile" className="github-image"/> {/* Use imported image */}
+          </a>
+          <a href="mailto:jhm359@cornell.edu" target="_blank" rel="noopener noreferrer">
+                <img src={imageSrc2} alt="Gmail" className="gmail-image"/> {/* Use imported image */}
+          </a>
+          <a href="https://www.linkedin.com/in/joshuahmcmahon/" target="_blank" rel="noopener noreferrer">
+                <img src={imageSrc3} alt="Linkedin Profile" className="linkedin-image"/> {/* Use imported image */}
+          </a>
+        </div>
     );
     
 }
