d76d2f4baa2561e409268b9e7f46ae1866e67758
Wizard10fun <wisard10fun@gmail.com>
2024-06-28 19:56:27 -0400

    Started working on projects page - added pomodoro app image - added pomodoro app description

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -2,6 +2,7 @@ import { useState, useEffect} from 'react';
 import BlogList from './BlogList';
 import useFetch from './useFetch';
 import imageSrc from './img/1705489908612.jpg'; // Import your image
+import imageSrc2 from './img/selfphotoflowers.jpg'; // Import your image
 
 //var counter = 0;
 
@@ -14,8 +15,8 @@ const AboutPage = () => {
         <div className="aboutPage">
             
             <h1 className="slideheaders">About</h1>
-            <div className="aboutpagecontainer">
-                <img src={imageSrc} alt="Joshua McMahon" className="profileimage"/> {/* Use imported image */}
+            <div className="pagecontainer">
+                <img src={imageSrc2} alt="" className="profileimage"/> {/* Use imported image */}
                 <body className="slidebody">
                     Hello, My name is Joshua McMahon! I am from Los Angeles, California and I am currently studying Computer 
                     Science at Cornell University. 
--- src/ProjectPage.js
+++ src/ProjectPage.js
@@ -1,4 +1,4 @@
-
+import imageSrc from './img/pomodoro-study-app-image.jpg'; // Import your image
 //var counter = 0;
 
 const ProjectPage = () => {
@@ -8,9 +8,15 @@ const ProjectPage = () => {
         <div className="projectPage">
             
             <h1 className="slideheaders">Projects</h1>
-            <body className="slidebody">
-                This is the project page
-            </body>
+            <div className="pagecontainer">
+                <img src={imageSrc} alt="Joshua McMahon" className="pomodoro-image"/> {/* Use imported image */}
+                <body className="slidebody">
+                    Pomodoro Study App: Study app that allows the user to create and organize tasks on different days. Also allows user to time their tasks
+ and set break times to promote studying efficiency. App tracks user data on study time and gives users points to
+ buy different virtual Koi fish for studying (users can view their Koi pond with all their fish swim around in it).
+ User can also view their own Analytics
+                </body>
+            </div>
            
         </div>
     );
